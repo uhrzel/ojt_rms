7e0f526b50fcc98d140b3ee9204d4fc16c2525fc
Arzel John Zolina <80521619+ArzelJrz10@users.noreply.github.com>
2023-08-15 19:54:38 +0800

    update getStudentTask

--- student/tasks.php
+++ student/tasks.php
@@ -6,6 +6,7 @@ $student_id = $_GET['student_id'];
 $organization_id = $_GET['organization_id'];
 
 $get_task = new Task();
-$tasks = $get_task->getTasks($student_id, $organization_id);
+$tasks = $get_task->getStudentTask($student_id, $organization_id);
+/* $tasks = $get_task->getTasks($student_id, $organization_id); */
 
 echo json_encode($tasks);
