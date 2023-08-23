1a01fb15a9c70da4dbfc00576d95d0763c3e1a0b
Arzel John Zolina <80521619+ArzelJrz10@users.noreply.github.com>
2023-08-23 14:52:27 +0800

    comments fix remaining hours

--- student/classes/Attendance.class.php
+++ student/classes/Attendance.class.php
@@ -3,8 +3,10 @@
 require_once('../config/Database.class.php');
 
 
-class Attendance extends Database{
-    public function getAttendance($student_id){
+class Attendance extends Database
+{
+    public function getAttendance($student_id)
+    {
         $sql = "SELECT * FROM tbl_attendance WHERE student_id = ? ORDER BY attendance_id DESC";
         $stmt = $this->connection->prepare($sql);
         $stmt->execute([$student_id]);
@@ -12,7 +14,8 @@ class Attendance extends Database{
         return $result;
     }
 
-    public function getTotalTrainingHours($student_id){
+    public function getTotalTrainingHours($student_id)
+    {
         $sql = "SELECT SUM(TIME_TO_SEC(TIMEDIFF(attendance_time_out, attendance_time_in))) AS total_hours FROM tbl_attendance WHERE student_id = ?";
         $stmt = $this->connection->prepare($sql);
         $stmt->execute([$student_id]);
@@ -24,13 +27,45 @@ class Attendance extends Database{
         $stmt->execute([$student_id]);
         $result = $stmt->fetch(PDO::FETCH_ASSOC);
         $remaining_hours = $result['required_hours'] - ($total_hours['total_hours'] / 3600);
-        
+
         // format remaining hours
         $remaining_hours = number_format($remaining_hours, 0, '.', '');
         return $remaining_hours;
     }
 
-    public function checkAttendance($student_id, $attendance_date){
+    //THIS COULD BE FIX THE GET TOTAL HOURS OF REMAINING HOURS
+    /*   public function getTotalTrainingHours($student_id)
+    {
+        $sql = "SELECT attendance_time_in, attendance_time_out FROM tbl_attendance WHERE student_id = ?";
+        $stmt = $this->connection->prepare($sql);
+        $stmt->execute([$student_id]);
+        $attendance_records = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        $total_seconds = 0;
+
+        foreach ($attendance_records as $record) {
+            $time_in = strtotime($record['attendance_time_in']);
+            $time_out = strtotime($record['attendance_time_out']);
+            $time_difference = $time_out - $time_in;
+            $total_seconds += $time_difference;
+        }
+
+        $total_hours = $total_seconds / 3600;
+
+        // get remaining hours
+        $sql = "SELECT * FROM tbl_student WHERE student_id = ?";
+        $stmt = $this->connection->prepare($sql);
+        $stmt->execute([$student_id]);
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        $remaining_hours = $result['required_hours'] - $total_hours;
+
+        // format remaining hours
+        $remaining_hours = number_format($remaining_hours, 0, '.', '');
+        return $remaining_hours;
+    }
+ */
+    public function checkAttendance($student_id, $attendance_date)
+    {
         $sql = "SELECT * FROM tbl_attendance WHERE student_id = ? AND attendance_date = ?";
         $stmt = $this->connection->prepare($sql);
         $stmt->execute([$student_id, $attendance_date]);
@@ -38,7 +73,8 @@ class Attendance extends Database{
         return $result;
     }
 
-    public function getAttendanceMorning($student_id, $attendance_date, $organization_id){
+    public function getAttendanceMorning($student_id, $attendance_date, $organization_id)
+    {
         $sql = "SELECT * FROM tbl_attendance WHERE student_id = ? AND attendance_date = ? AND attendance_log = 'Morning' AND organization_id = ?";
         $stmt = $this->connection->prepare($sql);
         $stmt->execute([$student_id, $attendance_date, $organization_id]);
@@ -46,7 +82,8 @@ class Attendance extends Database{
         return $result;
     }
 
-    public function getAttendanceAfternoon($student_id, $attendance_date, $organization_id){
+    public function getAttendanceAfternoon($student_id, $attendance_date, $organization_id)
+    {
         $sql = "SELECT * FROM tbl_attendance WHERE student_id = ? AND attendance_date = ? AND attendance_log = 'Afternoon' AND organization_id = ?";
         $stmt = $this->connection->prepare($sql);
         $stmt->execute([$student_id, $attendance_date, $organization_id]);
@@ -54,16 +91,18 @@ class Attendance extends Database{
         return $result;
     }
 
-    
-    
-    public function AttendanceTimeIn($student_id, $attendance_date, $attendance_time_in, $attendance_log, $coordinator_id, $organization_id){
+
+
+    public function AttendanceTimeIn($student_id, $attendance_date, $attendance_time_in, $attendance_log, $coordinator_id, $organization_id)
+    {
         $sql = "INSERT INTO tbl_attendance (student_id, attendance_date, attendance_time_in, attendance_log, coordinator_id, organization_id) VALUES (?, ?, ?, ?, ?, ?)";
         $stmt = $this->connection->prepare($sql);
         $result = $stmt->execute([$student_id, $attendance_date, $attendance_time_in, $attendance_log, $coordinator_id, $organization_id]);
         return $result;
     }
 
-    public function AttendanceTimeOut($attendance_id, $attendance_time_out){
+    public function AttendanceTimeOut($attendance_id, $attendance_time_out)
+    {
         $sql = "UPDATE tbl_attendance SET attendance_time_out = ? WHERE attendance_id = ?";
         $stmt = $this->connection->prepare($sql);
         $result = $stmt->execute([$attendance_time_out, $attendance_id]);
